8a66edbb25e0381f6eb8f62e88ec436406431cc5
Arinze <donariboy18@yahoo.com>
2022-12-01 01:56:56 -0500

    Twitter project

---
+++ Donari-Devs-Engrs/twitter.sh
@@ -0,0 +1,6 @@
+#This is a script on class 30 project for Twitter
+echo "Landmarktech is chnaging lives one at a time"
+echo "I am proud to be going through this program"
+echo "I love challenges, and this really challenges me"
+echo "Our project for Twitter will be successful"
+echo "And we will soon push it into the market"
